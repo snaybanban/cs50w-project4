1db1bb0b0f81cb63bc1787ad9bed318ec5b14d03
snaybanban <36524932+snaybanban@users.noreply.github.com>
2021-06-04 23:55:16 -0600

    Add files via upload

---
+++ COMERCIAL_AVDEL/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for COMERCIAL_AVDEL project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'COMERCIAL_AVDEL.settings')
+
+application = get_asgi_application()
---
+++ COMERCIAL_AVDEL/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for COMERCIAL_AVDEL project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'COMERCIAL_AVDEL.settings')
+
+application = get_wsgi_application()
